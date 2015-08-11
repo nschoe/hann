41863ece7aeaf6b070381fab7b5ab3ab7475b65e
nschoe <ns.schoe@gmail.com>
2015-08-11 19:11:35 +0200

    Implement 'mkNeuralNetwork' (ignoring Nguyen-Widrow)
    Nguyen-Widrow initialization method is left TODO for now, basic and
    normalized initialization methods are provided, which is enough for
    a start.
    
    Some additional type synonyms are provided for signature readability.
    
    To allow fast prototyping, default values are provided, which should
    allow anybody to quickly start using the library, these include:
    
     - defaultInitStrategy which defaults to normalized
    
     - defaultParameters which defaults to:
    
            * fixed learning rate of 1.0
    
            * mini-batch gradient descent, size 10
    
            * cross-entropy cost function

--- AI/HANN/FeedForward/Network.hs
+++ AI/HANN/FeedForward/Network.hs
@@ -3,6 +3,9 @@ module AI.HANN.FeedForward.Network
     -- * Types
       NeuralNetwork
     , Parameters(..)
+    , Structure
+    , ActivationFunction
+    , ActivationFunctionDeriv
     , LearningRateStrategy(..)
     , BackpropStrategy
     , WeightMatrix
@@ -10,6 +13,10 @@ module AI.HANN.FeedForward.Network
     , CostFunction(..)
     , TrainingExample
     , TrainingSet
+    , InitStrategy(..)
+
+    -- * Creating a neural network
+    , mkNeuralNetwork
 
     -- * Neural Network accessors
     , getStructure
@@ -23,7 +30,13 @@ module AI.HANN.FeedForward.Network
     , sigmoid'
     ) where
 
-import Numeric.LinearAlgebra.HMatrix (Matrix, Vector)
+import           Control.Monad                   (forM, replicateM)
+import           Control.Monad.Primitive         (PrimMonad, PrimState)
+import           Numeric.LinearAlgebra.HMatrix   (Matrix, Vector, (><))
+import           System.Random.MWC               (Gen, asGenST,
+                                                  withSystemRandom)
+import           System.Random.MWC.Distributions (normal, standard)
+import Data.Default
 
 -- |Core type of the library, describes a neural network's architecture
 data NeuralNetwork = NeuralNetwork {
@@ -33,17 +46,17 @@ data NeuralNetwork = NeuralNetwork {
     -- H1 neurons in the first hidden layer
     -- H2 neurons in the second hidden layer
     -- O output neurons
-      nnStructure :: [Int]
+      nnStructure       :: [Int]
 
     -- ^The neural network's weights
     -- Note that nnWeights[l] is the weight matrix from layer l to layer (l+1)
-    , nnWeights :: [WeightMatrix]
+    , nnWeights         :: [WeightMatrix]
 
     -- ^The units' biases
     -- Note that nnBiases[l] is the bias matrix of layer (l+1) (since first layer is input layer and has no biases)
-    , nnBiases :: [Bias]
-    , nnActivation :: Double -> Double
-    , nnActivationDeriv :: Double -> Double
+    , nnBiases          :: [Bias]
+    , nnActivation      :: ActivationFunction
+    , nnActivationDeriv :: ActivationFunctionDeriv
 }
 
 -- |Accessors for the @NeuralNetwork@ type
@@ -56,27 +69,40 @@ getWeights = nnWeights
 getBiases :: NeuralNetwork -> [Bias]
 getBiases = nnBiases
 
-getActivation :: NeuralNetwork -> (Double -> Double)
+getActivation :: NeuralNetwork -> ActivationFunction
 getActivation = nnActivation
 
-getActivationDeriv :: NeuralNetwork -> (Double -> Double)
+getActivationDeriv :: NeuralNetwork -> ActivationFunctionDeriv
 getActivationDeriv = nnActivationDeriv
 
 -- |Just type synonyms to make signatures more readable
-type WeightMatrix = Matrix Double
-type Bias = Matrix Double
+type Structure               = [Int]
+type WeightMatrix            = Matrix Double
+type Bias                    = Matrix Double
+type ActivationFunction      = Double -> Double
+type ActivationFunctionDeriv = Double -> Double
 
 -- |Regroups all parameters for the network
 data Parameters = Parameters {
       pLearningRateStrategy :: LearningRateStrategy -- ^Specifies the strategy used to update the learning rate
-    , pBackpropStrategy :: BackpropStrategy         -- ^Specifies the gradient descent strategy
-    , pCostFunction :: CostFunction                 -- ^Specifies the cost function to use for training
+    , pBackpropStrategy     :: BackpropStrategy     -- ^Specifies the gradient descent strategy
+    , pCostFunction         :: CostFunction         -- ^Specifies the cost function to use for training
 } deriving (Show, Eq)
+
+instance Default Parameters where
+    def = Parameters {
+      pLearningRateStrategy = def
+    , pBackpropStrategy     = def
+    , pCostFunction         = def
+}
 
 -- |The strategy to use for udpating the learning rate
 data LearningRateStrategy =
     FixedRate Double
     deriving (Show, Eq)
+
+instance Default LearningRateStrategy where
+    def = FixedRate 1.0
 
 -- |The strategy used for Gradient Descent
 data BackpropStrategy =
@@ -90,10 +116,16 @@ data BackpropStrategy =
     | OnlineGradientDescent
     deriving (Show, Eq)
 
+instance Default BackpropStrategy where
+    def = MiniBatchGradientDescent 10
+
 -- |Cost function type
 data CostFunction = MeanSquare
                   | CrossEntropy
     deriving (Show, Eq)
+
+instance Default CostFunction where
+    def = CrossEntropy
 
 -- |First element is the input, second is the expected result (target)
 type TrainingExample = (Vector Double, Vector Double)
@@ -109,3 +141,35 @@ sigmoid x = 1 / (1 + exp(-x))
 sigmoid' :: Double -> Double
 sigmoid' x = s * (1 - s)
     where s = sigmoid x
+
+-- |Different strategies for generating random values for weights and biases
+data InitStrategy = Basic         -- ^A gaussian distribution, with mean 0 and stdev 1 (not adapted to big networks)
+                  | Normalized    -- ^A gaussian distribution, with mean 0 and stdev 1/√(nbInputNeurons)
+                  | NguyenWidrow  -- ^A more sophisticated method, as seen [here](put link)
+    deriving (Show, Eq)
+
+instance Default InitStrategy where
+    def = Normalized
+
+-- |Creates a neural network. Ensures it is valid or fail
+mkNeuralNetwork :: InitStrategy -> ActivationFunction -> ActivationFunctionDeriv -> Structure -> IO NeuralNetwork
+mkNeuralNetwork _ _ _ struct | length struct < 2 = error "A neural network must have at least 2 layers (input and output)"
+                             | any (<= 0) struct = error "You can't have zero or a negative number of neurons in a layer"
+mkNeuralNetwork NguyenWidrow _ _ _ =
+    error "The Nguyen-Widrow random weight initialization method is not yet implemented, either use basic or normalized"
+mkNeuralNetwork initStrat h h' struct = do
+    basicWeights <- forM (zip struct (tail struct)) $ \(n1, n2) -> do
+        randomNumbers <- withSystemRandom . asGenST $ \gen -> replicateM (n1*n2) (distrib n1 gen)
+        return $ (n2><n1) randomNumbers
+    return NeuralNetwork {
+          nnStructure = struct
+        , nnWeights = basicWeights
+        , nnBiases = []
+        , nnActivation = h
+        , nnActivationDeriv = h'
+    }
+    where distrib :: PrimMonad m => Int -> Gen (PrimState m) -> m Double
+          distrib n1 = case initStrat of
+              Basic      -> standard -- standard generate a mean 0 and variance 1
+              Normalized -> normal 0 (1 / sqrt (fromIntegral n1))
+              _          -> error "mkNeuralNetwork (distrib): unsupported InitStrategy"
