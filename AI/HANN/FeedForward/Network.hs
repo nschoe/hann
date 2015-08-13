2124d8a5e3e2201511060af584e9615e3c85fa4e
nschoe <ns.schoe@gmail.com>
2015-08-13 18:37:28 +0200

    Implement basic training with backpropagation
    Implement generic training function 'trainUntil' which takes a
    predicate and keep training until the predicate returns True.
    
    Provide convenience functions 'trainNTimes' and
    'trainUntilErrorBelow' (in terms of 'trainUntil').
    
    Learning is bugged as of now: cost value is traced and is found to be
    _increasing_ across epochs.
    
    Debugging in progress.

--- AI/HANN/FeedForward/Network.hs
+++ AI/HANN/FeedForward/Network.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module AI.HANN.FeedForward.Network
     (
     -- * Types
@@ -14,6 +16,8 @@ module AI.HANN.FeedForward.Network
     , TrainingExample
     , TrainingSet
     , InitStrategy(..)
+    , Epoch
+    , Verbosity(..)
 
     -- * Creating a neural network
     , mkNeuralNetwork
@@ -31,16 +35,26 @@ module AI.HANN.FeedForward.Network
 
     -- * Running the neural network
     , runNN
+
+    -- * Training the neural network
+    , trainUntil
+    , trainNTimes
+
+    -- * Temporary
+    , test
     ) where
 
 import           Control.Monad                   (forM, replicateM)
 import           Control.Monad.Primitive         (PrimMonad, PrimState)
-import           Numeric.LinearAlgebra.HMatrix   (Matrix, Vector, (><), vector, (#>), cmap)
+import           Numeric.LinearAlgebra.HMatrix   (Matrix, Vector, (><), vector, (#>), cmap, tr, scale, asColumn, asRow, (<>), (|>), size, sumElements)
 import           System.Random.MWC               (Gen, asGenST,
                                                   withSystemRandom)
 import           System.Random.MWC.Distributions (normal, standard)
 import Data.Default
-import Data.List (foldl')
+import Data.List (foldl', scanl')
+import Data.List.Split (chunksOf)
+import Debug.Trace (trace)
+import System.IO (hSetBuffering, stdout, BufferMode(..))
 
 -- |Core type of the library, describes a neural network's architecture
 data NeuralNetwork = NeuralNetwork {
@@ -137,11 +151,14 @@ type TrainingExample = (Vector Double, Vector Double)
 -- |Defines a collection of training examples
 type TrainingSet = [TrainingExample]
 
+-- |Defines an epoch number, useful for signatures (in predicates for instance)
+type Epoch = Int
+
 -- |The sigmoid activation function
 sigmoid :: Double -> Double
 sigmoid x = 1 / (1 + exp(-x))
 
--- |The derivatives of the sigmoid activation function, expressed in terms of the sigmoid function
+-- |The derivatives of the sigmoid activation function
 sigmoid' :: Double -> Double
 sigmoid' x = s * (1 - s)
     where s = sigmoid x
@@ -154,6 +171,14 @@ data InitStrategy = Basic         -- ^A gaussian distribution, with mean 0 and s
 
 instance Default InitStrategy where
     def = Normalized
+
+-- |Allows for the training to report is progress
+-- Please note that they are inclusive (latters include formers)
+data Verbosity = Quiet
+               | EpochNumber
+               | CostOnEpoch
+               | CostOnBatch
+               deriving (Eq,Show,Ord)
 
 -- |Creates a neural network. Ensures it is valid or fail
 mkNeuralNetwork :: InitStrategy -> ActivationFunction -> ActivationFunctionDeriv -> Structure -> IO NeuralNetwork
@@ -179,7 +204,7 @@ mkNeuralNetwork initStrat h h' struct = do
           distrib n1 = case initStrat of
               Basic      -> standard -- standard generate a mean 0 and variance 1
               Normalized -> normal 0 (1 / sqrt (fromIntegral n1))
-              _          -> error "mkNeuralNetwork (distrib): unsupported InitStrategy"
+              _          -> error "mkNeuralNetwork > distrib: unsupported InitStrategy"
 
 -- |Runs the Neural Network on the input to get the output
 runNN :: NeuralNetwork -> Vector Double -> Vector Double
@@ -187,3 +212,141 @@ runNN nn input = foldl' (\a (w,b) -> cmap σ (w #> a + b)) input (zip weights bi
     where weights = nnWeights nn
           biases  = nnBiases nn
           σ       = nnActivation nn
+
+-- |Trains (with backpropagation) the Neural Network until the predicate returns true
+trainUntil :: (Epoch -> NeuralNetwork -> TrainingSet -> Bool) -> Parameters -> NeuralNetwork -> TrainingSet -> NeuralNetwork
+trainUntil predicate params nn trainingSet = train nn 0
+    where train :: NeuralNetwork -> Epoch -> NeuralNetwork
+          train nn' !epoch | predicate epoch nn' trainingSet = nn'
+                           | otherwise                       = let newNN = backProp epoch params nn' trainingSet
+                                                               in train newNN (epoch+1)
+
+-- |Performs one epoch of backpropagation on the Neural Network
+backProp :: Epoch -> Parameters -> NeuralNetwork -> TrainingSet -> NeuralNetwork
+backProp epoch params !nn trainingSet =
+    let (newWeights, newBiases) = foldl' (trainOnBatch epoch) (nnWeights nn, nnBiases nn) batches
+    -- in nn {nnWeights = newWeights, nnBiases = newBiases}
+        !newNN = nn {nnWeights = newWeights, nnBiases = newBiases}
+
+        -- outputs = map (runNN newNN) (map fst trainingSet)
+        -- !cost = (1.0 / (2.0 * fromIntegral (length trainingSet)) *) $ sum $ zipWith (\output target -> cmap (^2) (output - target)) outputs (map snd trainingSet)
+    in newNN
+    where batches = chunksOf batchSize trainingSet
+
+          batchSize = case pBackpropStrategy params of
+            BatchGradientDescent       -> length trainingSet
+            MiniBatchGradientDescent n -> n
+            OnlineGradientDescent      -> 1
+            --_                          -> error "backProp: unsupported Backprop strategy"
+
+          -- Accumulates gradient (error) on all examples in a batch
+          trainOnBatch :: Epoch -> ([WeightMatrix], [Bias]) -> TrainingSet -> ([WeightMatrix], [Bias])
+          trainOnBatch epoch (weights, biases) batch =
+              -- Create empty matrices and vectors with same size as the weights and biases, all initialized to 0
+              let (initialNabla_W, initialNabla_B) = (map (scale 0) weights, map (scale 0) biases)
+                  (nabla_W, nabla_B) = foldl' trainOnExample (initialNabla_W, initialNabla_B) batch
+
+                  updatedWeights = zipWith (\w nW -> w - scale (η / m) nW) weights nabla_W
+                  updatedBiases  = zipWith (\b nB -> b - scale (η / m) nB) biases nabla_B
+            --   in (updatedWeights, updatedBiases)
+                  !newNN = nn {nnWeights = updatedWeights, nnBiases = updatedBiases}
+                  outputs = map (runNN newNN) (map fst trainingSet)
+                  !cost = (1.0 / (2.0 * fromIntegral (length trainingSet)) *) $ sum $ zipWith (\output target -> cmap (^2) (output - target)) outputs (map snd trainingSet)
+
+              in trace ("#" ++ show epoch ++ " - " ++ show cost) $ (updatedWeights, updatedBiases)
+              where
+                -- Computes gradient (error) on one example of the batch
+                trainOnExample :: ([Matrix Double], [Vector Double]) -> TrainingExample -> ([Matrix Double], [Vector Double])
+                trainOnExample (accNabla_W, accNabla_B) (input, target) =
+                    -- Performs feed-forward, keeping both zs and as
+                    -- Input layer has no z value, so the initial left value in the tuple doesn't matter
+                    let forwardPass = scanl' feedForward (size input |> repeat 0.0, input) (zip weights biases)
+
+                    -- Computes the output error (last layer only)
+                        (zL, aL) = last forwardPass
+                        outputError = derivCost aL target * cmap σ' zL -- Hadamard product
+
+                    -- Backpropagates the error
+                    -- Don't take the first weight matrice, because we don't backpropagate the error to the input layer
+                    -- Don't take the last forwardPass because it is alreayd included in the initial accumulator
+                    -- don't take the first forwardPass because we don't backpropagate the error to the input layer
+                        δs = scanr backpropagate outputError (zip (tail weights) (map fst ((tail . init) forwardPass)))
+
+                    -- Accumulates the gradient
+                        accNabla_B' = zipWith (+) δs accNabla_B
+                        accNabla_W' = zipWith3 (\δ a nW -> nW + (asColumn δ <> asRow a) ) δs (map snd (init forwardPass)) accNabla_W
+
+                    in (accNabla_W', accNabla_B')
+
+                -- Performs the feed-forward pass, keeping both z and a values
+                feedForward :: (Vector Double, Vector Double) -> (WeightMatrix, Bias) -> (Vector Double, Vector Double)
+                feedForward (_, a) (w, b) = let newZ = (w #> a) + b
+                                                newA = cmap σ newZ
+                                            in (newZ, newA)
+
+                -- This is the gradient of the cost function
+                derivCost :: Vector Double -> Vector Double -> Vector Double
+                derivCost output target = case pCostFunction params of
+                    MeanSquare   -> target - output
+                    CrossEntropy -> error "backProp > trainOnBatch > trainOnExample > derivCost: Cross entropy function not handled yet"
+                    --_            -> error "backProp > trainOnBatch > trainOnExample > derivCost: unsupported cost function"
+
+                -- Backpropagates the error among the layer
+                backpropagate :: (WeightMatrix, Vector Double) -> Vector Double -> Vector Double
+                backpropagate (w, z) δ = ((tr w) #> δ) * cmap σ' z -- Hadamard product
+
+                -- Extracts the learning rate from the parameters
+                η :: Double
+                η = case pLearningRateStrategy params of
+                        FixedRate η' -> η'
+                        --_            -> error "backProp > trainOnBatch > trainOnExample > η: unsupported learning rate strategy"
+
+                -- Computes the length of the batch
+                m :: Double
+                m = (fromIntegral . length) batch
+
+          σ = nnActivation nn
+          σ' = nnActivationDeriv nn
+
+-- |Convenience function: trains for a fixed number of times
+trainNTimes :: Int -> Parameters -> NeuralNetwork -> TrainingSet -> NeuralNetwork
+trainNTimes n | n < 0     = error "trainNTimes: cannot train a negative number of times"
+              | otherwise = trainUntil (\epoch _ _ -> epoch == n)
+
+-- Convenience function: trains the network until the error reaches a threshold (limits the number of epochs)
+{-
+trainUntilErrorBelow :: Double -> Int -> Parameters -> NeuralNetwork -> TrainingSet -> NeuralNetwork
+trainUntilErrorBelow errThreshold maxEpochs
+    | maxEpochs < 0    = error "trainUntilErrorBelow: the maximum number of epochs cannot be negative"
+    | errThreshold < 0 = error "trainUntilErrorBelow: the error threshold cannot be negative"
+    | otherwise        = trainUntil predicate
+-}
+
+test :: IO ()
+test = do
+    hSetBuffering stdout NoBuffering
+    nn <- mkNeuralNetwork def sigmoid sigmoid' [2,2,1]
+    let input = [ vector [0,0]
+                , vector [0,1]
+                , vector [1,0]
+                , vector [1,1]
+                ] :: [Vector Double]
+        targets = [ vector [0]
+                  , vector [1]
+                  , vector [1]
+                  , vector [0]
+                  ] :: [Vector Double]
+        trainingSet = zip input targets
+
+    putStrLn "Initial output: "
+    mapM_ (print . asColumn . runNN nn) input
+    let params = Parameters {
+                       pLearningRateStrategy = FixedRate 3.0
+                     , pBackpropStrategy = BatchGradientDescent
+                     , pCostFunction = MeanSquare
+                 }
+
+        newNN = trainNTimes 1000 params nn trainingSet
+
+    putStrLn "\n===================\nFinal output: "
+    mapM_ (print . asColumn . runNN newNN) input
